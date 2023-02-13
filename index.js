e5cbc104567e8e8715a52877bce59c968a9c3424
Farhod <mrfarhod58@gmail.com>
2023-02-13 16:44:28 +0500

    image upload

--- index.js
+++ index.js
@@ -5,35 +5,60 @@ const expressEdge = require("express-edge")
 const mongoose = require("mongoose")
 const Post = require("./models/Post")
 const app = express()
+const fileUpload = require("express-fileupload")
 
+app.use(fileUpload())
 app.use(express.static("public"))
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
-
 app.use(expressEdge.engine)
+
+
 app.set("views", `${__dirname}/views`)
 
-app.get("/", (req, res)=>{
-    res.render("index")
+app.get("/", async (req, res)=>{
+    const posts = await Post.find().limit(5)
+    console.log(posts);
+    res.render("index", {posts: posts.reverse()})
 })
+
+app.get("/articles", async (req, res)=>{
+    const posts = await Post.find()
+
+    res.render("articles", {
+        posts: posts
+    })
+})
+
 app.get("/about", (req, res)=>{
     res.render("about")
 })
-app.get("/post", (req, res)=>{
-    res.render("post")
+app.get("/post/:id", async (req, res)=>{
+
+    const post = await Post.findById(req.params.id)
+
+    res.render("post", {
+        post: post
+    })
 })
 app.get("/contact", (req, res)=>{
     res.render("contact")
 })
-app.get("/posts/new", (req, res)=>{
+app.get("/posts/create", (req, res)=>{
     res.render("create")
 })
-app.post("/posts/new", (req, res)=>{
-    console.log(req.body);
-    Post.create(req.body, (err, data)=>{
-        err ? console.log(err) : console.log(data);
-        res.redirect("/posts/new")
-    })
+app.post("/posts/create", (req, res)=>{
+     const {image} =  req.files
+
+     image.mv(path.resolve(__dirname, "public/posts", image.name), (err)=>{
+        if(err){
+            console.log(err)
+        }
+        Post.create({...req.body, image: `posts/${image.name}`}, (err, data)=>{
+            err ? console.log(err) : console.log(data);
+            res.redirect("/posts/create")
+        }) 
+     })
 })
 
 
--- models/Post.js
+++ models/Post.js
@@ -3,7 +3,13 @@ const mongoose = require("mongoose")
 const PostSchema = new mongoose.Schema({
     title: String,
     description: String,
-    content: String
+    content: String, 
+    username: String,
+    createdAt: {
+        type: Date,
+        default: new Date()
+    },
+    image: String,
 })
 
 const Post = mongoose.model("Post", PostSchema)
